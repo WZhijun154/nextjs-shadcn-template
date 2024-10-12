c08cf1662f385f5f421ad5b589a129faf12cfea6
wangzhijun <wangzhijun154@gmail.com>
2024-10-12 22:55:42 +0800

    add not-found.tsx

---
+++ src/app/[lang]/not-found.tsx
@@ -0,0 +1,17 @@
+import { TopSection } from "@/components/section";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default async function NotFound() {
+  return (
+    <TopSection id="not-found" className="text-center">
+      <h1 className="custom-h1">404 - Page Not Found</h1>
+      <p className="custom-margin-after-title custom-muted-description">
+        The page you are looking for does not exist.
+      </p>
+      <Link href="/" className="custom-margin-after-description">
+        <Button>Go back to Home</Button>
+      </Link>
+    </TopSection>
+  );
+}
